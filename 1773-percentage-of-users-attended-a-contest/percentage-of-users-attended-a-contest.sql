6a3f81c226d3ce1fa80d20b854e5a1984854c80b
J Shireesha <23951a12d1@iare.ac.in>
2025-09-19 13:52:56 +0530

    Time: 1152 ms (21.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1773-percentage-of-users-attended-a-contest/percentage-of-users-attended-a-contest.sql
@@ -0,0 +1,10 @@
+select r.contest_id,
+ROUND(
+    (COUNT(DISTINCT r.user_id) /
+    (SELECT COUNT(u.user_id) From Users u))
+    *100, 2)
+    percentage
+    from users u join register r
+    on u.user_id=r.user_id
+    group by r.contest_id
+    order by percentage desc,contest_id;
