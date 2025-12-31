26d9ec3136e1423f7668fe999038d76119ff7d65
J Shireesha <23951a12d1@iare.ac.in>
2025-12-31 11:41:13 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (18.78%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if (n == 1) return nums[0];
+
+        // Case 1: Exclude last house
+        int case1 = robLinear(nums, 0, n - 2);
+
+        // Case 2: Exclude first house
+        int case2 = robLinear(nums, 1, n - 1);
+
+        return Math.max(case1, case2);
+    }
+
+    private int robLinear(int[] nums, int start, int end) {
+        int prev2 = 0; // dp[i-2]
+        int prev1 = 0; // dp[i-1]
+
+        for (int i = start; i <= end; i++) {
+            int curr = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
