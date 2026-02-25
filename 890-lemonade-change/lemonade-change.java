291aca0a30e411673562e913881fd65d5ff4522f
J Shireesha <23951a12d1@iare.ac.in>
2026-02-25 10:43:40 +0530

    Time: 2 ms (99.16%) | Memory: 72.6 MB (29.12%) - LeetSync

---
+++ 890-lemonade-change/lemonade-change.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+
+        int fiveDollarBills = 0;
+        int tenDollarBills = 0;
+
+        for (int customerBill : bills) {
+
+            if (customerBill == 5) {
+                fiveDollarBills++;
+
+            } else if (customerBill == 10) {
+                if (fiveDollarBills > 0) {
+                    fiveDollarBills--;
+                    tenDollarBills++;
+                } else {
+                    return false;
+                }
+
+            } else { // customerBill == 20
+                if (tenDollarBills > 0 && fiveDollarBills > 0) {
+                    tenDollarBills--;
+                    fiveDollarBills--;
+                } else if (fiveDollarBills >= 3) {
+                    fiveDollarBills -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
